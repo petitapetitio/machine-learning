b90866c17f73e5a8e44cd53c92ba8679f85087ce
lxnd <lxnd@protonmail.com>
2025-08-13 12:19:13 +0700

    cleanup files

---
+++ impact_of_feature_scaling_on_learning_curve.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+
+from aitk.feature_scaling import z_score_normalization
+from aitk.multiple_linear_regression import MultipleLinearProblemDataset, MultipleLinearModel, MultipleLinearRegression
+from datasets.dataset_2D_regression import x1, x2, y
+from numa.matrix import Matrix
+from numa.vector import Vector
+
+"""
+Le dataset non scalé converge rapidement AU DEBUT, mais peine dans les derniers 10%
+"""
+
+scaled_dataset = MultipleLinearProblemDataset.create(
+    Matrix.with_columns(Vector(z_score_normalization(x1)), Vector(z_score_normalization(x2))),
+    Vector(z_score_normalization(y))
+)
+unscaled_dataset = MultipleLinearProblemDataset.create(
+    Matrix.with_columns(Vector(x1), Vector(x2)),
+    Vector(y)
+)
+
+NB_ITERATIONS = 2000
+scaled_model = MultipleLinearModel(Vector.zeros(scaled_dataset.nb_features()), 0)
+unscaled_model = MultipleLinearModel(Vector.zeros(unscaled_dataset.nb_features()), 0)
+
+scaled_learning_rate = 0.4
+unscaled_learning_rate = 0.00004
+
+x = list(range(NB_ITERATIONS))
+learning_curve_scaled = [0] * NB_ITERATIONS
+learning_curve_unscaled = [0] * NB_ITERATIONS
+for i in range(NB_ITERATIONS):
+    learning_curve_scaled[i] = scaled_model.cost(scaled_dataset)
+    learning_curve_unscaled[i] = unscaled_model.cost(unscaled_dataset)
+    scaled_model = scaled_model.descent_gradient(scaled_dataset, learning_rate=scaled_learning_rate)
+    unscaled_model = unscaled_model.descent_gradient(unscaled_dataset, learning_rate=unscaled_learning_rate)
+
+
+fig = plt.figure(figsize=(12, 6))
+
+ax1 = fig.add_subplot(1, 1, 1)
+
+ax1.axhline(0, color='grey', linewidth=1)
+ax1.set_xlabel("iterations")
+ax1.set_ylabel("cost")
+ax1.plot(x, learning_curve_scaled, label=f"scaled", color="blue")
+
+ax2 = ax1.twinx()
+ax2.plot(x, learning_curve_unscaled, label=f"unscaled", color="orange")
+ax2.set_ylabel("cost (of the unscaled)")
+ax2.set_ylim(0, 1e8)
+
+fig.suptitle("Learning curves (scaled vs unscaled datasets)")
+plt.legend()
+plt.show()
---
+++ learning_curve.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+from aitk.feature_scaling import z_score_normalization
+from aitk.multiple_linear_regression import MultipleLinearProblemDataset, MultipleLinearModel, MultipleLinearRegression
+from datasets.dataset_2D_regression import x1, x2, y
+from numa.matrix import Matrix
+from numa.vector import Vector
+
+dataset = MultipleLinearProblemDataset.create(
+    Matrix.with_columns(Vector(z_score_normalization(x1)), Vector(z_score_normalization(x2))),
+    Vector(z_score_normalization(y))
+)
+
+NB_ITERATIONS = 2000
+model = MultipleLinearModel(Vector.zeros(dataset.nb_features()), 0)
+
+
+learning_rates = [0.01, 0.05, 0.1, 1]
+
+x = list(range(NB_ITERATIONS))
+
+learning_curves = []
+for learning_rate in learning_rates:
+    y = [0] * NB_ITERATIONS
+    for i in range(NB_ITERATIONS):
+        y[i] = model.cost(dataset)
+        model = model.descent_gradient(dataset, learning_rate=learning_rate)
+    learning_curves.append(y)
+
+
+fig = plt.figure(figsize=(12, 6))
+
+ax = fig.add_subplot(1, 1, 1)
+ax.axhline(0, color='grey', linewidth=1)
+ax.set_xlabel("iterations")
+ax.set_ylabel("cost")
+
+for alpha, learning_curve in zip(learning_rates, learning_curves):
+    ax.plot(x, learning_curve, label=f"alpha = {alpha}")
+title = fig.suptitle("Learning curves")
+
+plt.legend()
+plt.show()
